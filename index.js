fb92ec45674b9ea202664fa34000b90c094dce36
ydeeksha <75473851+ydeeksha@users.noreply.github.com>
2021-05-11 15:46:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+Webcam.attach( livecam );
+camera = document.getElementById("camera");
+Webcam.set({
+width  : 350,
+height : 300,
+image_format : 'png',
+png_quality : 90
+})
+
+function take_snapshot() {
+    Webcam.snap ( function (data_uri) {
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+ data_uri +'"/>';
+
+    });
+}
+console.log('ml5 version:', ml5.version);
+
+camera = document.getElementById("camera");
